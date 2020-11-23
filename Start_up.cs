b69e836a92e039590cb70e3f280fb611759fa5ff
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2020-11-23 09:58:49 +0700

    :octocat: Added Start
    :fire: Start => Change Object to invisible.

--- Start_up.cs
+++ Start_up.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 public class Change_Scene : MonoBehaviour
 {
   public GameObject name;
+  void Start(){
+    name.acctiveSelf(false);
+  }
   void OnCollisionEnter(Collision other){
     name.activeSelf(true);
  }
