e150224bc577e17f9e41ba8450c05951db5003ea
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2020-11-25 15:49:42 +0700

    :fire: Change UI to Collider

--- Quit.cs
+++ Quit.cs
@@ -6,15 +6,12 @@ using UnityEngine.UI;
 
 public class Quit : MonoBehaviour
 {
-    public Button ExitButton;
-    void Start()
-    {
-        Button x = ExitButton.GetComponent<Button>();
-        x.onClick.AddListener(Task);
-    }
     void Task()
     {
         Debug.Log("Exit!~~~~");
         Application.Quit();
+    }
+    void OnCollisionEnter(Collision other){
+        Task();       
     }
 }
