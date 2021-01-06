ca7c300876d8e2eb3a71a3a6c340b7fc58585b2e
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2021-01-06 18:31:15 +0700

    Update hand_spawn.cs

--- hand_spawn.cs
+++ hand_spawn.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using Valve.VR;
 
 public class hand_spawn : MonoBehaviour
 {
-  public GameObject Bullet;
-  public GameObject emp_hand;
-  public SteamVR_Input_Source Hand_Type;
-  public SteamVR_Action_Boolean Finger;
-  void Update(){
-    if(Finger.GetlastState(Hand_Type){
-      Instantiate(Bullet,emp_hand.transform.position);
+    public GameObject Bullet;
+    public GameObject emp_;
+    public SteamVR_Input_Source Hand_Type;
+    public SteamVR_Action_Boolean Finger;
+    void Update(){
+        if(Finger.GetLastState(Hand_Type)){
+            Instantiate(Bullet,emp_.transform.position);
+        }
     }
-  }
 }
