0351382205b54c8951719a4343213fe92124736a
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2021-01-19 14:25:09 +0700

    Update player_health.cs

--- health_sys/player_health.cs
+++ health_sys/player_health.cs
@@ -1,3 +1,4 @@
+  
 /*
  * ใส่ใน Player
  */
@@ -12,18 +13,33 @@ public class player_health : MonoBehaviour
     public int MaxHealth;
     public int x,y,z; //จุดเริ่มต้นของ Player
     private int health;
+    
+    public List<GameObject> health_items;
     void Start(){
         SetHealth(MaxHealth);
     }
-    void OnCollisionEnter(Collsion other){
-        if(other.gameObejct.tag == "Bullet"){
+    void OnCollisionEnter(Collision other){
+        if(other.gameObject.tag == "Bullet"){
             Destroy(other.gameObject);
             health -= damage;
+            remove_(health);
         }
     }
 
     void SetHealth(int MaxHealth){
         health = MaxHealth;
         gameObject.transform.position = new Vector3(x,y,z);
+        create();
+    }
+
+    void remove_(int it){
+        health_items[it].SetActive(false);
+    }
+    void create(){
+        int naz = 8;
+        for(int i=0;i<naz;++i){
+            health_items[i].SetActive(true);
+        }
+
     }
 }
