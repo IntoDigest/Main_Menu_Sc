fc8776b8a63dcf77b34dbbfabffa35efdc7cf012
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2021-01-19 13:52:28 +0700

    Update boss.cs

--- Final Boss/boss.cs
+++ Final Boss/boss.cs
@@ -2,14 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class Deduct : MonoBehaviour
-{  
+public class boss : MonoBehaviour
+{
     public int MxHealth;
     public static int boss_hp; //ไม่ต้องสนใจ
-    void Update(){
-        if(boss_hp <= 0){
-            //GameOver
+    public string next;
+    void Start(){
+        boss_hp = MxHealth;
+    }
+
+    void Update()
+    {
+        if (boss_hp <= 0)
+        {
+            SceneManager.LoadScene(next);
         }
     }
 }
